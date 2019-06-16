d7f3b3a70a156920a5af2740cc2aac052a3d8051
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2019-06-16 15:52:16 +0900

    python selection sort sample

---
+++ sort/SelectionSort_01.py
@@ -0,0 +1,19 @@
+import random
+
+
+def selectionSort(arr):
+    for i in range(len(arr), 0, -1):
+        max = 0
+
+        for j in range(0, i):
+            if arr[j] > arr[max]:
+                max = j
+
+        arr[max], arr[j] = arr[j], arr[max]
+
+    return arr
+
+
+arr = random.sample(range(1, 50), 7)  # 랜덤 배열 선언
+print(arr)  # 정렬 전
+print(selectionSort(arr))  # 정렬 후
