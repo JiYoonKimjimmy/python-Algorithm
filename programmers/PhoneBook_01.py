989d148fb4ec1092187e59bd597a69f759d681c7
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2020-04-10 21:20:58 +0900

    python study

--- programmers/PhoneBook_01.py
+++ programmers/PhoneBook_01.py
@@ -1,4 +1,6 @@
 def solution(phone_book):
+    # [프로그래머스 문제 : 전화번호 목록]
+    # - zip 내장함수를 통해서 첫번째 index 요소 비교
     phone_book = sorted(phone_book)
 
     for p1, p2 in zip(phone_book, phone_book[1:]):
